a11ae678e3c5c91a9e35a476c3fab51d83de237c
Emal Nawabi <emalnawabi2020@gmail.com>
2023-07-17 09:25:03 +0430

    adsense script added to _app components

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import NProgress from 'nprogress'
 import Router from 'next/router'
+import Script from 'next/script'
 import 'tailwindcss/tailwind.css'
 
 import '../styles/globals.css'
@@ -19,6 +20,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <MovieContextProvider>
       <Component {...pageProps} />
+      <Script
+        src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9893828741280282"
+      crossorigin="anonymous'
+        async
+      />
     </MovieContextProvider>
   )
 }
